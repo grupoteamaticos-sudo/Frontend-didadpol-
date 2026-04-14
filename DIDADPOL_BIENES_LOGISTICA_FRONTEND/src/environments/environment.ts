3f4fc8884bb2028d379aa3b0042d694222b771b0
GRUPO PROYECTO UNAH <grupoteamaticos@gmail.com>
2026-04-14 14:02:32 -0600

    Fix: update apiUrl to production render url

--- DIDADPOL_BIENES_LOGISTICA_FRONTEND/src/environments/environment.ts
+++ DIDADPOL_BIENES_LOGISTICA_FRONTEND/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8091/api',
+  production: true,
+  apiUrl: 'https://backend-didadpol.onrender.com/api' // URL de Render
 };
